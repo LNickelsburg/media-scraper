960eb8863975f839db43368bc8d81991029ad3d1
LNickelsburg <l.nickelsburg@gmail.com>
2024-05-23 09:58:46 -0400

    use csv instead of txt

--- test_scraper.py
+++ test_scraper.py
@@ -1,42 +1,42 @@
-import praw # type: ignore
-import csv
-
-
-'''run this separately for each subreddit so u dont get rate limited'''
+import praw
+import pandas as pd
 
 reddit_read_only = praw.Reddit(client_id = 'Dwr17XS-Rc4T4WETNJF9_w',
                                client_secret = 'kRmJ1cu1rtjYpe3J2YL02pQANNb6zQ',
                                user_agent = 'web:osint_scraper:v1 (by u/notaflightlessbird)')
 
-'''subreddit_list = ['gazainvasionfootage',
-                    'israelpalestine', 
-                    'israelwarvideoreport', 
-                    'israel_palestine',
-                    'osint',
-                    'palestine', 
-                    '2ndyomkippurwar']
-'''
+subreddit_list = ['gazainvasionfootage',
+                  'israelpalestine', 
+                  'israelwarvideoreport', 
+                  'israel_palestine',
+                  'osint',
+                  'palestine', 
+                  '2ndyomkippurwar']
 
-subreddits_dict = {"Display Name": [], "Title": [], 
-                   "Description": [], "Subscribers": [], 
-                   "Posts": []}
+all_posts = []
 
-name = '2ndyomkippurwar'
-subreddit = reddit_read_only.subreddit(name)
-subreddits_dict["Display Name"].append(subreddit.display_name)
-subreddits_dict["Title"].append(subreddit.title)
-subreddits_dict["Description"].append(subreddit.description)
-subreddits_dict["Subscribers"].append(subreddit.subscribers)
+for name in subreddit_list:
+    subreddit = reddit_read_only.subreddit(name)
+    for post in subreddit.top(limit=100):
 
-subreddit_posts = []
-for post in subreddit.top(limit=100):
-    subreddit_posts.append((post.url, post.title, post.selftext, post.score, post.num_comments))
-
-
-subreddits_dict["Posts"].append(subreddit_posts)
-
-filename = "./posts/" + name + "_posts.txt"
-file = open(filename, "w", encoding='utf-8')
-for post_list in subreddits_dict["Posts"]:
-    for post in post_list:
-        file.write(str(post) + "\n")
+        # collect comments
+        post.comments.replace_more(limit=100)
+        comments = []
+        for comment in post.comments.list():
+            comments.append(comment.body)
+        
+        # add comments and post info to list
+        all_posts.append({
+            "subreddit": name,
+            "url": post.url,
+            "title": post.title,
+            "selftext": post.selftext,
+            "score": post.score,
+            "id": post.id,
+            "num_comments": post.num_comments,
+            "comments": comments,
+            "created": post.created
+            })
+        
+df = pd.DataFrame(all_posts)
+df.to_csv('reddit_data.csv', index=False, encoding='utf-8')
